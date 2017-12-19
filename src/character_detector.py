018a4eb6a088698ed9e2297b74e2f1f7c51293e7
Alexander Chiu <al.chiu407@gmail.com>
2017-12-18 17:15:58 -0800

    Create a mask for word segmentation

--- src/character_detector.py
+++ src/character_detector.py
@@ -4,17 +4,33 @@ This module contains the necessary scripts to detect characters and merge
 bouding boxes of the characters.
 
 """
-import os
 import argparse
+from operator import itemgetter
 import cv2
 import numpy as np
-from operator import itemgetter
+
+
+def create_word_mask(boxes, height, width):
+    """Create a mask for our words"""
+    # Create a blank mask
+    mask = np.zeros((height, width), dtype="uint8")
+
+    # Using the bounding boxes, we will draw white boxes on the mask
+    for box in boxes:
+        cv2.rectangle(mask, (box[2], box[3]),
+                      (box[2]+box[4], box[3]+box[5]),
+                      (255, 255, 255), -1)
+
+    # With this mask, we will have to unidirectionally dilate so that
+    # characters within the word are connected.
+    return mask
+
 
 def merge_boxes(contours, height, width):
     """Merge the bounding boxes
 
     Input -- list of character bounding boxes, image height, and image width
-    Output -- list of new word bounding boxes
+    Output -- list of new character bounding boxes for special characters
     """
     boxes = []
     merged = []
@@ -29,6 +45,9 @@ def merge_boxes(contours, height, width):
 
     # Need to sort this list by x-coordinate
     sorted_boxes = sorted(boxes, key=itemgetter(2))
+
+    # Need to merge boxes of multi-component
+    # characters i, j, !, ?, :, ;, =, %, and ".
     return sorted_boxes
 
 
@@ -53,27 +72,24 @@ def detector_for_words(image):
 
     # Apply Otsu binarization thresholding on the blurred image
     _, otsu_image = cv2.threshold(blur_image, 0, 255,
-                                    cv2.THRESH_BINARY_INV
-                                    + cv2.THRESH_OTSU)
+                                  cv2.THRESH_BINARY_INV
+                                  + cv2.THRESH_OTSU)
 
     # With the otsu_image, we can start creating bounding boxes of words
     _, contours, _ = cv2.findContours(otsu_image,
-                                                   cv2.RETR_EXTERNAL,
-                                                   cv2.CHAIN_APPROX_SIMPLE)
+                                      cv2.RETR_EXTERNAL,
+                                      cv2.CHAIN_APPROX_SIMPLE)
 
     # Let's draw the contours to see what we have
     boxes = merge_boxes(contours, resized_height, resized_width)
-    for box in boxes:
-        cv2.rectangle(resized_image, (box[2], box[3]),
-                      (box[2]+box[4], box[3]+box[5]),
-                      (0, 255, 0), 2)
+    mask = create_word_mask(boxes, resized_height, resized_width)
+    masked_image = cv2.bitwise_and(resized_image, resized_image, mask=mask)
 
-    # Write and show this new image for testing
-    temp_image = "{}.png".format(os.getpid())
-    cv2.imwrite(temp_image, resized_image)
-    cv2.imshow("bounding_boxes", resized_image)
+    # Show this new image for testing
+    cv2.imshow("bounding_boxes", masked_image)
     cv2.waitKey(0)
-    os.remove(temp_image)
+
+    return boxes
 
 
 if __name__ == "__main__":
