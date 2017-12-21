b02d1a82a87d53e8d5adf2d655188b3d48972831
Alexander Chiu <al.chiu407@gmail.com>
2017-12-20 17:48:39 -0800

    Merge multi-component characters

--- src/character_detector.py
+++ src/character_detector.py
@@ -17,13 +17,35 @@ def create_word_mask(boxes, height, width):
 
     # Using the bounding boxes, we will draw white boxes on the mask
     for box in boxes:
-        cv2.rectangle(mask, (box[2], box[3]),
-                      (box[2]+box[4], box[3]+box[5]),
+        cv2.rectangle(mask, (box[0], box[1]),
+                      (box[0]+box[2], box[1]+box[3]),
                       (255, 255, 255), -1)
 
     # With this mask, we will have to unidirectionally dilate so that
     # characters within the word are connected.
     return mask
+
+
+def sort_contours(contours):
+    """Sort the contours by x and y coordinates
+     and calculate the central coordinates"""
+    boxes = []
+    for contour in contours:
+        [x_coord, y_coord, width, height] = cv2.boundingRect(contour)
+        center_x = x_coord + width / 2
+        center_y = y_coord + height / 2
+        box = [center_x, center_y, x_coord, y_coord, width, height]
+        boxes.append(box)
+
+    # Need to sort this list by x and y-coordinates
+    boxes = sorted(boxes, key=itemgetter(2, 3))
+
+    return boxes
+
+
+def dis(x, y):
+    """Calculate the Euclidean distance in one direction"""
+    return abs(x - y)
 
 
 def merge_boxes(contours, height, width):
@@ -32,23 +54,56 @@ def merge_boxes(contours, height, width):
     Input -- list of character bounding boxes, image height, and image width
     Output -- list of new character bounding boxes for special characters
     """
-    boxes = []
+    # Thresholds for checking multi-component characters
+    h1 = .12 * height
+    h2 = .05 * height
+    w1 = .05 * width
+
+    # New list of bounding boxes
     merged = []
 
-    # Produce a list of bounding box coordinates with their centers
-    for contour in contours:
-        [x_coord, y_coord, width, height] = cv2.boundingRect(contour)
-        center_x = x_coord + width / 2
-        center_y = y_coord + height / 2
-        box = [center_x, center_y, x_coord, y_coord, width, height]
-        boxes.append(box)
-
-    # Need to sort this list by x-coordinate
-    sorted_boxes = sorted(boxes, key=itemgetter(2))
+    # Produce a sorted list of bounding box coordinates with their centers
+    box = sort_contours(contours)
 
     # Need to merge boxes of multi-component
     # characters i, j, !, ?, :, ;, =, %, and ".
-    return sorted_boxes
+    empty = (0, 0, 0, 0, 0, 0)
+    prev_list = [empty] + box
+    aft_list = box + [empty]
+
+    for bef, cur, aft in zip(prev_list, box, aft_list):
+        # First check the current and previous box
+        if(dis(cur[0], bef[0]) <= w1) and (h2 < dis(cur[1], bef[1]) <= h1):
+            # Check three boxes for the % character
+            if(dis(cur[0], aft[0]) <= w1) and (h2 < dis(cur[1], aft[1] <= h1)):
+                # We have a potential % character
+                new_x = min(bef[2], cur[2], aft[2])
+                new_y = min(bef[3], cur[3], aft[3])
+                new_w = max(bef[4], cur[4], aft[4])
+                new_h = max(bef[5], cur[5], aft[5])
+            else:
+                new_x = min(bef[2], cur[2])
+                new_y = min(bef[3], cur[3])
+                # Check for the quotation mark
+                if(dis(cur[1], bef[1]) <= h2):
+                    new_w = bef[4] + cur[4]
+                    new_h = max(bef[5], cur[5])
+                else:
+                    # The next set of characters are the vertical
+                    # two-component characters
+                    # NEED TO ASSOCIATE THE MIN_Y HEIGHT
+                    new_w = max(bef[4], cur[4])
+                    new_h = bef[5] + cur[5] + (max(bef[3], cur[3])
+                                               - (new_y + min(bef[5], cur[5])))
+        # Stand alone character
+        else:
+            new_x = cur[2]
+            new_y = cur[3]
+            new_w = cur[4]
+            new_h = cur[5]
+        merged.append([new_x, new_y, new_w, new_h])
+
+    return merged
 
 
 def detector_for_words(image):
