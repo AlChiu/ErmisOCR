956cb74d14662ffad5849474157d3b09255f7377
Alexander <al.chiu407@gmail.com>
2018-01-01 22:16:22 -0800

    Segment character images from word images

--- src/segmentor.py
+++ src/segmentor.py
@@ -1,22 +1,25 @@
 """Segmentation
 
 """
+import os
 import cv2
 
 
-def segment(image, boxes, setting):
+def segment(filename, image, boxes, setting):
     """Used to crop out images based on the bounding boxes"""
     for i, box in enumerate(boxes):
-        crop_image = image[box[3]:box[3]+box[5],
-                           box[2]:box[2]+box[4]]
+        # Write to file name based on the segment setting
+        if setting == "word":
+            crop_image = image[box[3]:box[3]+box[5],
+                               box[2]:box[2]+box[4]]
+            crop_file = "word_" + str(i) + ".png"
+        else:
+            crop_image = image[box[1]:box[1]+box[3],
+                               box[0]:box[0]+box[2]]
+            path = os.path.splitext(os.path.basename(filename))[0]
+            crop_file = "char_" + str(i) + "_" + path + ".png"
+
         border_crop = cv2.copyMakeBorder(crop_image, 5, 5, 5, 5,
                                          cv2.BORDER_CONSTANT,
                                          value=(255, 255, 255))
-
-        # Write to file name based on the segment setting
-        if setting == "word":
-            crop_file = "word_" + str(i) + ".png"
-        else:
-            crop_file = "char_" + str(i) + ".png"
-
         cv2.imwrite(crop_file, border_crop)
