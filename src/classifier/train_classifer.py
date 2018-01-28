49b2eb7eb8731800aeaadf5059d20c4b6e53a4ab
Alexander Chiu <al.chiu407@gmail.com>
2018-01-28 15:19:25 -0800

    Train model over the multiple subsets of the dataset

--- src/classifier/train_classifer.py
+++ src/classifier/train_classifer.py
@@ -4,17 +4,23 @@ data, build the neural network graph, then train. It will
 return and save the model as a .h5 file
 """
 import os
-import time
+from pathlib import Path
+import matplotlib.pyplot as plt
 import random
 import argparse
 import numpy as np
 import keras
-from keras import backend as K
+from keras.models import load_model
+from keras.callbacks import History
 import model
 import convert_data
 
+HISTORY = History()
 HEIGHT = 32
 WIDTH = 32
+DIVISION = 10
+BATCH_SIZE = 200
+EPOCHS = 50
 
 
 def create_feed_data(directory, train_pkl_no, test_pkl_no):
@@ -61,21 +67,71 @@ def create_feed_data(directory, train_pkl_no, test_pkl_no):
             # test_lbl.append(keras.utils.to_categorical(line[2], 62))
 
         # Reshape and create one-hot vectors for labels
-        print('{} training images and {} testing images'.format(len(train_img),
-                                                                len(test_img)))
+        print('> {} training images loaded'.format(len(train_img)))
+        print('> {} testing images loaded'.format(len(test_img)))
 
         train_img = np.array(train_img)
         train_lbl = keras.utils.to_categorical(train_lbl, 62)
         test_img = np.array(test_img)
         test_lbl = keras.utils.to_categorical(test_lbl, 62)
 
+        # Reshape the image arrays into numpy arrays for the network
         train_img = train_img.reshape(train_img.shape[0], HEIGHT, WIDTH, 1)
         test_img = test_img.reshape(test_img.shape[0], HEIGHT, WIDTH, 1)
 
     except FileNotFoundError:
-        print('Pickle files were not found in {}'.format(directory))
+        print('> Pickle files were not found in {}'.format(directory))
 
     return train_img, train_lbl, test_img, test_lbl
+
+
+def build_model():
+    """Build the Keras neural network"""
+    print("> Building Keras neural network...")
+    network_model = model.build_model()
+    return network_model
+
+
+def fit_model(model, train_img, train_lbl, test_img, test_lbl, name):
+    """
+    Train the neural network with batch size of 200, 100 epochs,
+    and print out training progress.
+    """
+    model.fit(
+        train_img,
+        train_lbl,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        verbose=1,
+        validation_data=(test_img, test_lbl),
+        callbacks=[HISTORY]
+    )
+
+    score = model.evaluate(test_img, test_lbl, verbose=0)
+    print('Test loss: ', score[0])
+    print('Test accuaracy: ', score[1])
+
+    model.save(name)
+    return model
+
+
+def plot(model, setting, count):
+    """Plot the accuracy and loss of training"""
+    fig = setting + '_' + str(count) + '_graph.png'
+    if setting == "accuracy":
+        plt.plot(model.History['acc'])
+        plt.plot(model.History['val_acc'])
+        plt.title('Model Accuracy')
+        plt.ylabel('Accuracy')
+    elif setting == "loss":
+        plt.plot(model.History['loss'])
+        plt.plot(model.History['val_loss'])
+        plt.title('Model Loss')
+        plt.ylabel('Loss')
+
+    plt.xlabel('Epoch')
+    plt.legend(['Training', 'Testing'], loc='upper left')
+    plt.savefig(fig)
 
 
 if __name__ == "__main__":
@@ -88,4 +144,34 @@ if __name__ == "__main__":
     #############################
     # Feed Test
     #############################
-    create_feed_data(ARGS['directory'], 0, 0)
+    MODEL_NAME = 'model_62char.h5'
+    DIRECTORY = ARGS['directory']
+    MODEL_DIRECTORY = os.path.join(DIRECTORY, MODEL_NAME)
+
+    COUNT = 0
+    while COUNT < DIVISION:
+        train_img, train_lbl, test_img, test_lbl = create_feed_data(DIRECTORY,
+                                                                    COUNT,
+                                                                    COUNT)
+        MODEL_FILE = Path(MODEL_DIRECTORY)
+        if MODEL_FILE.is_file():
+            char_model = load_model(MODEL_DIRECTORY)
+            history = fit_model(char_model,
+                                train_img,
+                                train_lbl,
+                                test_img,
+                                test_lbl,
+                                MODEL_DIRECTORY)
+            # plot(history, "accuracy", COUNT)
+            # plot(history, "loss", COUNT)
+        else:
+            char_model = build_model()
+            history = fit_model(char_model,
+                                train_img,
+                                train_lbl,
+                                test_img,
+                                test_lbl,
+                                MODEL_DIRECTORY)
+            # plot(history, "accuracy", COUNT)
+            # plot(history, "loss", COUNT)
+        COUNT += 1
