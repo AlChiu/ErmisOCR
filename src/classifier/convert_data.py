6b7af8c4c658d91100ce4e3062aebb779a54f39b
Alexander Chiu <al.chiu407@gmail.com>
2018-02-11 17:08:40 -0800

    Resize images to 224 x 224, remove pickling

--- src/classifier/convert_data.py
+++ src/classifier/convert_data.py
@@ -5,16 +5,15 @@ We use the NIST SD19 by_class dataset
 import os
 import time
 import math
-import pickle
+# import pickle
 import argparse
 import cv2
-from PIL import Image
 from scipy import ndimage
 import numpy as np
 import nist_by_class
 
-HEIGHT = 28
-WIDTH = 28
+HEIGHT = 224
+WIDTH = 224
 LABELS = {
     '0': 0,
     '1': 1,
@@ -136,8 +135,8 @@ def resize_images(directory):
                 # Grab the filename
                 fname = os.path.splitext(image_path)[0] + '_' + label + '.png'
 
-                if width != WIDTH | height != HEIGHT:
-                    # Resize the image from 128 x 128 to 28 x 28 using
+                if width != WIDTH or height != HEIGHT:
+                    # Resize the image from 128 x 128 to 32 x 32 using
                     # Lecun's method seen in the MNIST dataset
 
                     (thresh, image) = cv2.threshold(image, 127, 255,
@@ -159,23 +158,23 @@ def resize_images(directory):
 
                     rows, cols = image.shape
 
-                    # Resize the resulting image into a 20 x 20 image
+                    # Resize the resulting image into a 28 x 28 image
                     # while maintaining the aspect ratio
                     if rows > cols:
-                        factor = 20.0 / rows
-                        rows = 20
+                        factor = 214.0 / rows
+                        rows = 214
                         cols = int(round(cols * factor))
                     else:
-                        factor = 20.0 / cols
-                        cols = 20
+                        factor = 214.0 / cols
+                        cols = 214
                         rows = int(round(rows * factor))
                     image = cv2.resize(image, (cols, rows))
 
-                    # Pad the 20 x 20 so that it is now 28 x 28
-                    column_padding = (int(math.ceil((28-cols)/2.0)),
-                                      int(math.ceil((28-cols)/2.0)))
-                    row_padding = (int(math.ceil((28-rows)/2.0)),
-                                   int(math.ceil((28-rows)/2.0)))
+                    # Pad the 28 x 28 so that it is now 32 x 32
+                    column_padding = (int(math.ceil((224-cols)/2.0)),
+                                      int(math.ceil((224-cols)/2.0)))
+                    row_padding = (int(math.ceil((224-rows)/2.0)),
+                                   int(math.ceil((224-rows)/2.0)))
                     image = np.lib.pad(image,
                                        (row_padding, column_padding),
                                        'constant')
@@ -186,7 +185,6 @@ def resize_images(directory):
                     image = shifted
 
                     # Save the image
-                    image = cv2.bitwise_not(image)
                     cv2.imwrite(fname, image)
                     os.remove(image_path)
                     print('{} resized'.format(item))
@@ -199,165 +197,138 @@ def resize_images(directory):
     return directory
 
 
-def convert_to_pixel_array(image_path):
-    """
-    Convert an image into a 2D array of pixel intensities, standardized
-    and zero-centered by subtracting each pixel value by the image mean
-    and dividing it by the stardard deviation.
-    Input: Absolute image path
-    Output: Resized, normalized pixel array of the image
-    """
-    pixels = []
+# def generate_label_list(directory):
+#     """
+#     Generate a text file that lists all of the image paths with their label
+#     Input: NIST_split directory (after running nist_by_class script)
+#     Output: text file of image paths with their label
+#     """
+#     start = time.time()
 
-    iamge = Image.open(image_path)
-    pixels = list(iamge.getdata())
+#     # Change to the directory
+#     os.chdir(directory)
 
-    # Normalize and zero center pixel data
-    std_dev = np.std(pixels)
-    mean = np.mean(pixels)
+#     # List out subset folders
+#     root_dirc = os.listdir()
 
-    # Each pixel will have the image mean substracted from it.
-    # Then divide the result by the image standard deviation.
-    # Replace that result as the new pixel value
-    pixels = [(pixels[offset:offset+WIDTH] - mean)/std_dev for offset in
-              range(0, WIDTH*HEIGHT, WIDTH)]
+#     for subset in root_dirc:
+#         if os.path.isdir(subset):
+#             label_path = os.path.join(directory, subset)
+#             labels = os.listdir(label_path)
 
-    # Convert the pixel list into a numpy array
-    pixels = np.array(pixels).astype(np.float32)
+#             train_file = "char_train_label_" + str(subset) + ".txt"
+#             test_file = "char_test_label_" + str(subset) + ".txt"
 
-    return pixels
+#             train_file = open(os.path.join(directory, train_file), "w+")
+#             test_file = open(os.path.join(directory, test_file), "w+")
 
+#             for label in labels:
+#                 folder_path = os.path.join(label_path, label)
+#                 folders = os.listdir(folder_path)
 
-def generate_label_list(directory):
-    """
-    Generate a text file that lists all of the image paths with their label
-    Input: NIST_split directory (after running nist_by_class script)
-    Output: text file of image paths with their label
-    """
-    start = time.time()
+#                 for folder in folders:
+#                     image_path = os.path.join(folder_path, folder)
+#                     images = os.listdir(image_path)
 
-    # Change to the directory
-    os.chdir(directory)
+#                     for image in images:
+#                         img_path = os.path.join(image_path, image)
+#                         if folder == 'training':
+#                             train_file.write(str(image) +
+#                                              "," +
+#                                              str(img_path) +
+#                                              "," +
+#                                              str(LABELS.get(str(label))) +
+#                                              "\r\n")
+#                         if folder == 'testing':
+#                             test_file.write(str(image) +
+#                                             "," +
+#                                             str(img_path) +
+#                                             "," +
+#                                             str(LABELS.get(str(label))) +
+#                                             "\r\n")
 
-    # List out subset folders
-    root_dirc = os.listdir()
+#     train_file.close()
+#     test_file.close()
 
-    for subset in root_dirc:
-        if os.path.isdir(subset):
-            label_path = os.path.join(directory, subset)
-            labels = os.listdir(label_path)
-
-            train_file = "char_train_label_" + str(subset) + ".txt"
-            test_file = "char_test_label_" + str(subset) + ".txt"
-
-            train_file = open(os.path.join(directory, train_file), "w+")
-            test_file = open(os.path.join(directory, test_file), "w+")
-
-            for label in labels:
-                folder_path = os.path.join(label_path, label)
-                folders = os.listdir(folder_path)
-
-                for folder in folders:
-                    image_path = os.path.join(folder_path, folder)
-                    images = os.listdir(image_path)
-
-                    for image in images:
-                        img_path = os.path.join(image_path, image)
-                        if folder == 'training':
-                            train_file.write(str(image) +
-                                             "," +
-                                             str(img_path) +
-                                             "," +
-                                             str(label) +
-                                             "\r\n")
-                        if folder == 'testing':
-                            test_file.write(str(image) +
-                                            "," +
-                                            str(img_path) +
-                                            "," +
-                                            str(label) +
-                                            "\r\n")
-
-    train_file.close()
-    test_file.close()
-
-    # Time
-    print('> Label files complete: {}'.format(time.time() - start))
-    return directory
+#     # Time
+#     print('> Label files complete: {}'.format(time.time() - start))
+#     return directory
 
 
-def pickle_data(directory, text_file_no, datafile, setting):
-    """
-    Read a text file and pickle the contents to prevent work repeat
-    Input: Text file containing the filename, absolute image path, label
-    Output: Pickled file of the character data
-    """
-    start = time.time()
-    # Point is a dictionary that contains the image file name, its path,
-    # the label, and the actual image pixel array.
+# def pickle_data(directory, text_file_no, datafile, setting):
+#     """
+#     Read a text file and pickle the contents to prevent work repeat
+#     Input: Text file containing the filename, absolute image path, label
+#     Output: Pickled file of the character data
+#     """
+#     start = time.time()
+#     # Point is a dictionary that contains the image file name, its path,
+#     # the label, and the actual image pixel array.
 
-    # Char_data is the dictionary of all points from the label text file
-    char_data = []
+#     # Char_data is the dictionary of all points from the label text file
+#     char_data = []
 
-    # First, change to the dataset root directory
-    os.chdir(directory)
+#     # First, change to the dataset root directory
+#     os.chdir(directory)
 
-    # Find the text file
-    if setting == "training":
-        text_file = "char_train_label_subset_" + str(text_file_no) + ".txt"
-    elif setting == "testing":
-        text_file = "char_test_label_subset_" + str(text_file_no) + ".txt"
-    else:
-        print("We need the correct setting")
+#     # Find the text file
+#     if setting == "training":
+#         text_file = "char_train_label_subset_" + str(text_file_no) + ".txt"
+#     elif setting == "testing":
+#         text_file = "char_test_label_subset_" + str(text_file_no) + ".txt"
+#     else:
+#         print("We need the correct setting")
 
-    # Open the text file
-    if text_file:
-        try:
-            with open(text_file) as text:
-                for line in text:
-                    # Remove the new line character from each line
-                    text_line = line.strip().split(',')
-                    image = convert_to_pixel_array(text_line[1])
-                    text_line.append(image)
-                    text_line.append(LABELS.get(str(text_line[2])))
-                    char_data.append(text_line)
-            pickle.dump(char_data, open(datafile, "wb"),
-                        protocol=pickle.HIGHEST_PROTOCOL)
-        except FileNotFoundError:
-            print('Text file {} not foud'.format(text_file))
+#     # Open the text file
+#     if text_file:
+#         try:
+#             with open(text_file) as text:
+#                 for line in text:
+#                     # Remove the new line character from each line
+#                     text_line = line.strip().split(',')
+#                     image = cv2.normalize(cv2.imread(text_line[1]),
+#                                           None, alpha=0, beta=1,
+#                                           norm_type=cv2.NORM_MINMAX,
+#                                           dtype=cv2.CV_32F)
+#                     char_data.append(LABELS.get(str(text_line[2])))
+#                     char_data.append(cv2.resize(image, (224, 224)))
+#             pickle.dump(char_data, open(datafile, "wb"),
+#                         protocol=pickle.HIGHEST_PROTOCOL)
+#         except FileNotFoundError:
+#             print('Text file {} not foud'.format(text_file))
 
-    print("> Pickle time: {}".format(time.time() - start))
+#     print("> Pickle time: {}".format(time.time() - start))
 
 
-def load_data(directory, datafile_no, setting):
-    """
-    Function to load a pickled datafile and return its contents
-    Input: Directory to the pickle file, subset no, setting(testing/training)
-    Output: Contents of specified pickle file
-    """
-    os.chdir(directory)
+# def load_data(directory, datafile_no, setting):
+#     """
+#     Function to load a pickled datafile and return its contents
+#     Input: Directory to the pickle file, subset no, setting(testing/training)
+#     Output: Contents of specified pickle file
+#     """
+#     os.chdir(directory)
 
-    # Based on setting, load the specified pkl file
-    if setting == "testing":
-        fname = "test_pickle_" + str(datafile_no) + ".pkl"
-    elif setting == "training":
-        fname = "train_pickle_" + str(datafile_no) + ".pkl"
-    else:
-        print("The setting must be specified as testing or training")
+#     # Based on setting, load the specified pkl file
+#     if setting == "testing":
+#         fname = "test_pickle_" + str(datafile_no) + ".pkl"
+#     elif setting == "training":
+#         fname = "train_pickle_" + str(datafile_no) + ".pkl"
+#     else:
+#         print("The setting must be specified as testing or training")
 
-    # Unpickle the pickle
-    if fname:
-        try:
-            data = pickle.load(open(fname, "rb"))
-            print('Data loaded from {}'.format(fname))
-        except FileNotFoundError:
-            print('{} not found'.format(fname))
-            print('Creating {}'.format(fname))
-            pickle_data(directory, datafile_no, fname, setting)
-            data = pickle.load(open(fname, "rb"))
-            print('Data loaded from {}'.format(fname))
+#     # Unpickle the pickle
+#     if fname:
+#         try:
+#             data = pickle.load(open(fname, "rb"))
+#             print('Data loaded from {}'.format(fname))
+#         except FileNotFoundError:
+#             print('{} not found'.format(fname))
+#             print('Creating {}'.format(fname))
+#             pickle_data(directory, datafile_no, fname, setting)
+#             data = pickle.load(open(fname, "rb"))
+#             print('Data loaded from {}'.format(fname))
 
-    return data
+#     return data
 
 
 if __name__ == "__main__":
@@ -371,24 +342,11 @@ if __name__ == "__main__":
     # Main pipeline test
     #############################
 
-    # First, resize the images
+    # # First, resize the images
     RESIZED = resize_images(ARGS['directory'])
 
-    # Second, spilt the dataset into multiple subsets
+    # # Second, spilt the dataset into multiple subsets
     SPLIT, DIVISION = nist_by_class.split_NIST(RESIZED, 10)
 
-    # Third, generate the label text files
-    DATA = generate_label_list(SPLIT)
-
-    # Pickle the ten subsets
-    COUNT = 0
-    while COUNT < DIVISION:
-        TRAINNAME = "train_pickle_" + str(COUNT) + ".pkl"
-        TESTNAME = "test_pickle_" + str(COUNT) + ".pkl"
-        pickle_data(DATA, COUNT, TRAINNAME, "training")
-        pickle_data(DATA, COUNT, TESTNAME, "testing")
-        COUNT += 1
-
-    # # Load the pickle data
-    TRAIN = load_data(DATA, 9, "training")
-    TEST = load_data(DATA, 9, "testing")
+    # # Third, generate the label text files
+    # DATA = generate_label_list(SPLIT)
