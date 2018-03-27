491949450332dadc6f2cf2e206858f957477514b
Alexander <al.chiu407@gmail.com>
2018-03-27 12:33:56 -0700

    Load json file for label dictionary

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -3,75 +3,10 @@ Classifier class definition
 """
 import os
 import glob
-import argparse
+import json
 import cv2
 from keras.models import load_model
 from src.detector import char_detect_segment as det_seg
-
-CHAR_LABELS = {
-    0: '0',
-    1: '1',
-    2: '2',
-    3: '3',
-    4: '4',
-    5: '5',
-    6: '6',
-    7: '7',
-    8: '8',
-    9: '9',
-    10: 'a',
-    11: 'b',
-    12: 'c',
-    13: 'd',
-    14: 'e',
-    15: 'f',
-    16: 'g',
-    17: 'h',
-    18: 'i',
-    19: 'j',
-    20: 'k',
-    21: 'l',
-    22: 'm',
-    23: 'n',
-    24: 'o',
-    25: 'p',
-    26: 'q',
-    27: 'r',
-    28: 's',
-    29: 't',
-    30: 'u',
-    31: 'v',
-    32: 'w',
-    33: 'x',
-    34: 'y',
-    35: 'z',
-    36: 'A',
-    37: 'B',
-    38: 'C',
-    39: 'D',
-    40: 'E',
-    41: 'F',
-    42: 'G',
-    43: 'H',
-    44: 'I',
-    45: 'J',
-    46: 'K',
-    47: 'L',
-    48: 'M',
-    49: 'N',
-    50: 'O',
-    51: 'P',
-    52: 'Q',
-    53: 'R',
-    54: 'S',
-    55: 'T',
-    56: 'U',
-    57: 'V',
-    58: 'W',
-    59: 'X',
-    60: 'Y',
-    61: 'Z'
-}
 
 
 def preprocess(image_path):
@@ -107,7 +42,7 @@ class Classifier:
     Defines the classifier for image prediction.
     Needs a trained neural model to function properly.
     """
-    def __init__(self, model):
+    def __init__(self, model, lab_dict):
         """
         Initialize the character classifier with the
         specified trained neural network model.
@@ -116,6 +51,12 @@ class Classifier:
             self.model = load_model(model)
         else:
             raise ValueError('A trained model must be provided.')
+
+        if lab_dict is not None:
+            with open(lab_dict) as file:
+                self.labels = json.load(file)
+        else:
+            raise ValueError('A label dictionary is required')
 
     def classify_one(self, image_path):
         """
@@ -129,13 +70,11 @@ class Classifier:
             image = image.reshape(-1, 224, 224, 1)
             outp = self.model.predict(image)[0]
 
-            top_idx = (-outp).argsort()[:1]
+            top_idx = outp.argmax(axis=-1)
 
-            for char in top_idx:
-                character = CHAR_LABELS.get(char)
+            character = self.labels.get(top_idx)
 
             return character
-
         else:
             raise ValueError('A trained model must be provided')
 
