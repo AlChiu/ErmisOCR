e9d4f6be4002297385e78127ba9f8d187fdddff6
Alexander Chiu <al.chiu407@gmail.com>
2018-03-26 17:21:59 -0700

    Remove VGG16, use RMSProp for MobileNet

--- training/model.py
+++ training/model.py
@@ -15,92 +15,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings('ignore')
 
 
-def vgg16_net(classes=62):
-    """
-    Model #5: VGGNet16 -3
-    """
-    model = Sequential()
-
-    # BLOCK 1
-    model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 1),
-                     padding='same', name='blk1_conv1'))
-    model.add(Conv2D(64, (3, 3), input_shape=(224, 224, 1),
-                     padding='same', name='blk1_conv1'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(32, (3, 3), padding='same', name='blk1_conv2'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='blk1_pool'))
-
-    # BLOCK 2
-    model.add(Conv2D(64, (3, 3), padding='same', name='blk2_conv1'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(64, (3, 3), padding='same', name='blk2_conv2'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='blk2_pool'))
-
-    # BLOCK 3
-    model.add(Conv2D(128, (3, 3), padding='same', name='blk3_conv1'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(128, (3, 3), padding='same', name='blk3_conv2'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(128, (3, 3), padding='same', name='blk3_conv3'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='blk3_pool'))
-
-    # BLOCK 4
-    model.add(Conv2D(256, (3, 3), padding='same', name='blk4_conv1'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(256, (3, 3), padding='same', name='blk4_conv2'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(256, (3, 3), padding='same', name='blk4_conv3'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='blk4_pool'))
-
-    # BLOCK 5
-    model.add(Conv2D(512, (3, 3), padding='same', name='blk5_conv1'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(512, (3, 3), padding='same', name='blk5_conv2'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(512, (3, 3), padding='same', name='blk5_conv3'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='blk5_pool'))
-
-    # CLASSIFICATION
-    model.add(Flatten(name='flatten'))
-    model.add(Dense(units=1024, name='fc1'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(rate=0.5))
-    model.add(Dense(units=1024, name='fc2'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(rate=0.5))
-    model.add(Dense(units=classes, name='classifier'))
-    model.add(BatchNormalization())
-    model.add(Activation('softmax'))
-
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.SGD(lr=1e-3,
-                                                 momentum=0.9,
-                                                 decay=1e-4,
-                                                 nesterov=True),
-                  metrics=['accuracy'])
-    return model
-
-
 def mobile_net(alpha=1, classes=62):
     """
     Google's Mobile Net modified to use ELU activation.
@@ -168,10 +82,10 @@ def mobile_net(alpha=1, classes=62):
     model.add(Reshape((classes,)))
 
     model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.SGD(lr=1e-3,
-                                                 momentum=0.9,
-                                                 decay=1e-4,
-                                                 nesterov=True),
+                  optimizer=keras.optimizers.rmsprop(lr=0.001,
+                                                     rho=0.9,
+                                                     epsilon=None,
+                                                     decay=0.0),
                   metrics=['accuracy'])
     return model
 
