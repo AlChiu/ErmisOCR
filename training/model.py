0c8f0ff282b9e4f152db56e9cb8587ae8fa9c8ab
Alexander Chiu <al.chiu407@gmail.com>
2018-04-18 20:52:14 -0700

    Remove VGG-based and simple network

--- training/model.py
+++ training/model.py
@@ -74,11 +74,6 @@ def mobile_net(classes, height, width, alpha=1):
     # shape = (1, 1, int(1024 * alpha))
 
     model.add(GlobalAveragePooling2D())
-    # model.add(Reshape(shape))
-    # model.add(Dropout(rate=1e-3))
-    # model.add(Conv2D(classes, (1, 1), padding='same'))
-    # model.add(Activation('softmax'))
-    # model.add(Reshape((classes,)))
     model.add(Dense(classes, activation='softmax'))
 
     model.compile(loss=keras.losses.categorical_crossentropy,
@@ -86,36 +81,6 @@ def mobile_net(classes, height, width, alpha=1):
                                                      rho=0.9,
                                                      epsilon=None,
                                                      decay=0.0),
-                  metrics=['accuracy'])
-    return model
-
-
-def simple_net(classes, height, width):
-    """
-    Simple Toy ConvNet for testing
-    """
-    model = Sequential()
-
-    # Conv1 Block
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same',
-                     input_shape=(width, height, 1)))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    # Conv2 Block
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    # FC1 Block
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(400, activation='relu'))
-
-    # Classifier Block
-    model.add(Dense(classes, activation='softmax'))
-
-    # Compile the model
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.rmsprop(lr=0.001),
                   metrics=['accuracy'])
     return model
 
@@ -165,44 +130,99 @@ def lenet(classes, height, width):
     return model
 
 
-def vgg_esque(classes, height, width):
+def lenet_2(classes, height, width):
     """
-    VGG based with bottlenecking
+    LeNet-5_2 Based classifier
     """
     inp_shape = (width, height, 1)
     model = Sequential()
 
-    # Conv Block 1
-    model.add(Conv2D(128, (3, 3), activation='relu',
+    # Conv1-Input Layer
+    model.add(Conv2D(32, (3, 3),
+                     activation='relu',
                      input_shape=inp_shape,
-                     padding='same'))
-    model.add(Conv2D(128, (3, 3), activation='relu',
-                     padding='same'))
+                     kernel_initializer='he_normal'))
+
+    # Pooling Layer 1
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    # Conv Block 2
-    model.add(Conv2D(256, (3, 3), activation='relu',
-                     padding='same'))
-    model.add(Conv2D(256, (3, 3), activation='relu',
-                     padding='same'))
-    model.add(Conv2D(256, (3, 3), activation='relu',
-                     padding='same'))
+    # Conv2 Layer
+    model.add(Conv2D(64, (3, 3),
+                     activation='relu',
+                     kernel_initializer='he_normal'))
+
+    # Pooling Layer 2
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    # Conv3 Layer
+    model.add(Conv2D(128, (1, 1), activation='relu',
+                     kernel_initializer='he_normal'))
 
     # Fully Connected 1
     model.add(Flatten())
-    model.add(Dense(750, activation='relu'))
+    model.add(Dense(400, activation='relu', kernel_initializer='he_normal'))
     model.add(Dropout(0.5))
 
     # Fully Connected 2
-    model.add(Dense(500, activation='relu'))
+    model.add(Dense(200, activation='relu', kernel_initializer='he_normal'))
     model.add(Dropout(0.5))
 
     # Classifier
-    model.add(Dense(classes, activation='softmax'))
+    model.add(Dense(classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal'))
 
     # Compile
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adagrad',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+    return model
+
+
+def lenet_3(classes, height, width):
+    """
+    LeNet-5_2 Based classifier
+    """
+    inp_shape = (width, height, 1)
+    model = Sequential()
+
+    # Conv1-Input Layer
+    model.add(Conv2D(16, (5, 5),
+                     activation='relu',
+                     input_shape=inp_shape,
+                     kernel_initializer='he_normal'))
+
+    # Pooling Layer 1
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    # Conv2 Layer
+    model.add(Conv2D(32, (3, 3),
+                     activation='relu',
+                     kernel_initializer='he_normal'))
+
+    # Pooling Layer 2
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    # Conv3 Layer
+    model.add(Conv2D(64, (1, 1), activation='relu',
+                     kernel_initializer='he_normal'))
+
+    # Fully Connected 1
+    model.add(Flatten())
+    model.add(Dense(300, activation='relu', kernel_initializer='he_normal'))
+    model.add(Dropout(0.5))
+
+    # Fully Connected 2
+    model.add(Dense(150, activation='relu', kernel_initializer='he_normal'))
+    model.add(Dropout(0.5))
+
+    # Classifier
+    model.add(Dense(classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal'))
+
+    # Compile
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
                   metrics=['accuracy'])
     return model
